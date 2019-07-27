3f89ae8cee812e8ee96d2a003b2b54cff18756e2
Meinolf Droste <mdwp.post@gmail.com>
2019-07-27 14:20:51 +0200

    initial deploy

---
+++ src/Objs/Redirect/RedirectEditingConfig.js
@@ -0,0 +1,18 @@
+import * as Scrivito from "scrivito";
+import redirectObjIcon from "../../assets/images/redirect_obj.svg";
+
+Scrivito.provideEditingConfig("Redirect", {
+  title: "Redirect",
+  thumbnail: redirectObjIcon,
+  hideInSelectionDialogs: true,
+  attributes: {
+    title: {
+      title: "Title",
+      description: "Limit to 55 characters.",
+    },
+    link: {
+      title: "Link",
+    },
+  },
+  properties: ["title", "link"],
+});
