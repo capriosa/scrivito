bacc962ce4830e9055f1667c5a18bc1e11fd1e43
Meinolf Droste <mdwp.post@gmail.com>
2019-08-17 20:10:24 +0200

    blog

--- src/Objs/Blog/BlogComponent.js
+++ src/Objs/Blog/BlogComponent.js
@@ -3,7 +3,7 @@ import * as Scrivito from "scrivito";
 import BlogPost from "../BlogPost/BlogPostObjClass";
 import navigateToBlogWithTag from "../../utils/navigateToBlogWithTag";
 import SchemaDotOrg from "../../Components/SchemaDotOrg";
-import TagList from "../../Components/TagList";
+//import TagList from "../../Components/TagList";
 
 Scrivito.provideComponent("Blog", ({ page, params }) => {
   const tags = [...BlogPost.all().facet("tags")].map(facet => facet.name());
@@ -11,12 +11,7 @@ Scrivito.provideComponent("Blog", ({ page, params }) => {
 
   return (
     <div>
-      <TagList
-        tags={tags}
-        showTags
-        setTag={navigateToBlogWithTag}
-        currentTag={currentTag}
-      />
+
       <Scrivito.ContentTag className="div" content={page} attribute="body" />
       <SchemaDotOrg content={page} />
     </div>
